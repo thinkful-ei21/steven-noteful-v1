bae05778d121b8c2c4d1a20d4a56b2d037c59b5b
Steven Imus <imussg@gmail.com>
2018-05-21 22:55:12 -0400

    completed get call code w/ ID in url

--- server.js
+++ server.js
@@ -1,7 +1,38 @@
 'use strict';
 
+const express = require('express');
 // Load array of notes
 const data = require('./db/notes');
+
+const app = express();
+
+// ADD STATIC SERVER HERE
+
+app.listen(8080, function() {
+	console.info(`Server listening on ${this.address().port}`);
+}).on('error', err => {
+	console.error(err);
+});
+
+app.get('/api/notes', (req, res) => {
+	res.json(data);
+});
+
+const findNote = function(id) {
+	console.log(`the ID being "found": ${id}, and typeof: ${typeof id}`);
+	for(let i=0; i<data.length; i++) {
+		console.log(`"data" note ID: ${data[i].id}, and typeof: ${typeof data[i].id}`);
+		if(id == data[i].id) {
+			return data[i];
+		}
+	}
+	return {error: "ID not found"};
+};
+
+app.get('/api/notes/:id', (req, res) => {
+	// console.log(1002 == req.params.id);
+	res.json(findNote(req.params.id));
+});
 
 console.log('Hello Noteful!');
 
