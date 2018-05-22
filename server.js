21f77d48906cd020a8395c2fe1e8ad1e69d9111b
Steven Imus <imussg@gmail.com>
2018-05-22 13:46:49 -0400

    modularized the port

--- server.js
+++ server.js
@@ -6,11 +6,13 @@ const data = require('./db/notes');
 
 const app = express();
 
+const { PORT } = require('./config');
+
 // ADD STATIC SERVER HERE
 app.use(express.static('public'));
 
 
-app.listen(8080, function() {
+app.listen(PORT , function() {
 	console.info(`Server listening on ${this.address().port}`);
 }).on('error', err => {
 	console.error(err);
