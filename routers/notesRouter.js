9ea0bf2c818a6ffab80d472f6ccc0b19400ea5f5
Steven Imus <imussg@gmail.com>
2018-05-23 22:22:10 -0400

    modularized server and substituted the custom logger for morgan

--- routers/notesRouter.js
+++ routers/notesRouter.js
@@ -1,19 +1,12 @@
-'use strict';
-
 const express = require('express');
-// Load array of notes
-const data = require('./db/notes');
-const { requestLogger } = require('./middleware/logger');
+const router = express.Router();
 
-const app = express();
+const bodyParser = require('body-parser');
+const jsonParser = bodyParser.json();
 
-const { PORT } = require('./config');
+const data = require('../db/notes');
 
-app.use(requestLogger);
-// ADD STATIC SERVER HERE
-app.use(express.static('public'));
-
-app.get('/api/notes', (req, res) => {
+router.get('/', (req, res) => {
 	res.json(data);
 });
 
@@ -28,21 +21,21 @@ const findNote = function(id) {
 	return {error: "ID not found"};
 };
 
-app.get('/api/notes/:id', (req, res) => {
+router.get('/:id', (req, res) => {
 	res.json(findNote(req.params.id));
 });
 
-app.get('/boom', (req, res, next) => {
+router.get('/boom', (req, res, next) => {
   throw new Error('Boom!!');
 });
 
-app.use(function (req, res, next) {
+router.use(function (req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   res.status(404).json({ message: 'Not Found' });
 });
 
-app.use(function (err, req, res, next) {
+router.use(function (err, req, res, next) {
   res.status(err.status || 500);
   res.json({
     message: err.message,
@@ -50,10 +43,4 @@ app.use(function (err, req, res, next) {
   });
 });
 
-app.listen(PORT , function() {
-	console.info(`Server listening on ${this.address().port}`);
-}).on('error', err => {
-	console.error(err);
-});
-
-console.log('Hello Noteful!');
+module.exports = router;
